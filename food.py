aaca24e8df92ac39a89957f3337b8b31ac0e1b4c
Jiwook Seo <spjo21@naver.com>
2018-12-31 18:50:16 +0000

    weekly updates : timezone error fix, week change

--- food.py
+++ food.py
@@ -13,22 +13,24 @@ s = requests.Session()
 res = s.post(LOGIN_URL, data=LOGIN_DATA, verify=False, allow_redirects=False)
 # print(res.raise_for_status())
 
-url = 'https://edu.ssafy.com/edu/board/notice/detail.do?brdItmSeq=331'
-res2 = s.get(url).text
-doc = bs(res2, 'html.parser')
-result_food=doc.find_all("table")
-foodlist=result_food[1]
+url = 'https://edu.ssafy.com/edu/board/notice/detail.do?brdItmSeq=521'
+res = s.get(url).text
+doc = bs(res, 'html.parser')
+result=doc.find_all("table")
+foodlist=result[1]
 trs = foodlist.find_all('tr')
+#data scraping
 
-days=trs[0].text
+days=trs[2].text # 주차 별로 데이터가 들어있는 인덱스가 달랐다?
 days=days.split("\n")
 days=days[2:-1]
 days_array=np.array(days)
 for i in range(5):
     days_array[i]=days_array[i][1:]
+#날짜데이터 가져오기
 
 menu=[]
-for i in range(2,20):
+for i in range(4,20): # 주차 별로 데이터가 들어있는 인덱스가 달랐다?
     menu.append(trs[i].text.split("\n")[1:-1])
 menu[0]=menu[0][3:]
 menu[8]=menu[8][1:]
@@ -41,10 +43,13 @@ menu_dict={}
 for i in range(5):
     menu_dict[i]=menu_array[i].reshape((2,8))
 
-
 def foodMsg(chat_name, chat_id, day):
-    dt = datetime.datetime.now()
+    dt = datetime.datetime.now(datetime.timezone.utc)
+    tz = datetime.timezone(datetime.timedelta(hours=7))
+    dt = dt.astimezone(tz)
+    #timezone을 설정하여 한국 시간대에 맞출 수 있도록
     wd=dt.weekday()
+    print(dt)
     check=True
     if day=="내일":
         wd+=1
@@ -62,15 +67,19 @@ def foodMsg(chat_name, chat_id, day):
             getTelegram(sendParams(chat_id,"월-금요일 식단만 확인 가능합니다."))
             check=False
     if check:
+        
         message=""
         message+="{}님 안녕하세요 \n\n".format(chat_name)
-        message+="{}의 A형 식단은\n".format(days_array[wd])
-        for i in range(7):
-            message+=menu_dict[wd][0][i][1:]+"\n"
-        message+="총 칼로리는 {}입니다.\n\n".format(menu_dict[wd][0][-1][1:])
-        message+="{}의 B형 식단은\n".format(days_array[wd])
-        for i in range(7):
-            message+=menu_dict[wd][1][i][1:]+"\n"
-        message+="총 칼로리는 {}입니다.\n\n".format(menu_dict[wd][1][-1][1:])
+        if menu_dict[wd][0][-1][1:]=="\u3000":
+            message+="{}은 즐거운 휴일입니다.\n푹 쉬세요 :)\n".format(days_array[wd])
+        else:
+            message+="{}의 A형 식단은\n".format(days_array[wd])
+            for i in range(7):
+                message+=menu_dict[wd][0][i][1:]+"\n"
+            message+="총 칼로리는 {}입니다.\n\n".format(menu_dict[wd][0][-1][1:])
+            message+="{}의 B형 식단은\n".format(days_array[wd])
+            for i in range(7):
+                message+=menu_dict[wd][1][i][1:]+"\n"
+            message+="총 칼로리는 {}입니다.\n\n".format(menu_dict[wd][1][-1][1:])
         message+="인싸봇 올림. 좋은 하루 되세요 :)"
         getTelegram(sendParams(chat_id,message))
