799648144e9fd20ed17e425a0062be78693eca41
Jiwook Seo <spjo21@naver.com>
2019-01-03 06:40:10 +0000

    request, help, admin message function add

--- app.py
+++ app.py
@@ -32,8 +32,10 @@ def delete_webhook():
 def telegram():
     doc = request.get_json()
     chatName, chatId, chatMsg = getData(doc)
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     if chatMsg[-2:] == "식단" or chatMsg[-2:] == "ㅅㄷ":
         foodMsg(chatName, chatId, chatMsg[0:2])
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     elif chatMsg == "알림해제" or chatMsg == "알림 해제" or chatMsg == "알림취소" or chatMsg == "알림 취소" or chatMsg == "취소" or chatMsg == "구독 취소" or chatMsg == "구독취소":
         with open('except.csv', 'r') as f:
             except_id = csv.reader(f)
@@ -45,6 +47,7 @@ def telegram():
             a=csv.writer(f)
             a.writerow(except_list)
         getTelegram(sendParams(chatId,"{} 님 구독 취소되었습니다.".format(chatName)))
+
     elif chatMsg == "알림설정" or chatMsg == "알림 설정" or chatMsg == "구독" or chatMsg == "구독 설정" or chatMsg == "구독설정":
         with open('except.csv', 'r') as f:
             except_id = csv.reader(f)
@@ -58,16 +61,45 @@ def telegram():
             a=csv.writer(f)
             a.writerow(except_list)
         getTelegram(sendParams(chatId,"{} 님 구독 설정되었습니다.".format(chatName)))
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    elif chatMsg == "문의" or chatMsg == "문의 사항"or chatMsg == "문의사항":
+        msg=""
+        msg+="'[문의]' 태그를 붙이고 궁금하신 점을 말씀해주시면 빠른 시일 내에 답변해드립니다."
+        getTelegram(sendParams(chatId,msg))
+        
+    elif chatMsg == "요청" or chatMsg == "요청사항" or chatMsg == "요청 사항":
+        msg=""
+        msg+="'[요청]' 태그를 붙이고 궁금하신 점을 말씀해주시면 향후 업데이트에 반영해드립니다."
+        getTelegram(sendParams(chatId,msg))
+    
+    elif chatMsg[:4]=="[문의]":
+        getTelegram(sendParams(chatId,"{}님 감사합니다.\n빠른 시일 내에 답변해드리겠습니다.".format(chatName)))
+        getTelegram(sendParams(tele_myid,chatMsg+str(chatId)+chatName))
+        
+    elif chatMsg[:4]=="[요청]":
+        getTelegram(sendParams(chatId,"{}님 좋은 의견 감사합니다.\n향후 업데이트에 참고토록 하겠습니다.".format(chatName)))
+        getTelegram(sendParams(tele_myid,chatMsg+str(chatId)+chatName))
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     elif chatMsg == "커리큘럼" or chatMsg == "수업" or chatMsg == "커리" or chatMsg == "수업 일정" or chatMsg == "수업일정" or chatMsg == "일정" or chatMsg == "ㅋㄹㅋㄹ":
         curMsg(chatName, chatId)
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    elif chatMsg[:3]=="msg" and str(chatId)==str(tele_myid):
+        admin=chatMsg.split(",")
+        getTelegram(sendParams(admin[1],admin[2]))
+        getTelegram(sendParams(tele_myid,"{}님께 {} 메세지 보내기 완료".format(admin[1],admin[2])))
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
     else:
         msg="잘못된 명령어입니다.\n\n"
         msg+="사용 가능한 명령어 :\n"
         msg+="식단 > 어제, 오늘, 내일, 모레 식단 출력 기능\n"
         msg+="구독 취소 > 매일 아침, 식단 정기알림 구독을 취소합니다.\n"
-        msg+="구독 설정 > 매일 아침, 식단 정기알림 구독을 신청합니다.(default)"
+        msg+="구독 설정 > 매일 아침, 식단 정기알림 구독을 신청합니다.(default)\n"
         msg+="커리큘럼 or 수업일정 > 이번 주 커리큘럼 출력 기능\n"
+        msg+="문의, 요청 사항 > 궁금하신 점이나, 필요한 기능이 있으면 알려주세요.\n"
         getTelegram(sendParams(chatId,"{} 님 죄송합니다. {}".format(chatName,msg)))
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     with open('data.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
     data[chatId]=chatName
